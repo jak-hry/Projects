e6137244a852547154fd4cf3c476cd7458f30588
Jakub Hryckiewicz <jak-hry@wp.pl>
2022-12-09 16:41:52 +0100

    testy HashMapy

--- TicTacToe-project/src/main/java/GameStats/GameStatsMain.java
+++ TicTacToe-project/src/main/java/GameStats/GameStatsMain.java
@@ -8,7 +8,10 @@ public class GameStatsMain {
     public static void main(String[] args) {
 
         ArrayList<Integer> arrayList = new ArrayList<>();
+        //licznik wygranych pierwszego gracza
         arrayList.add(1);
+        //licznik wygranych drugiego gracza
+        arrayList.add(2);
 
         HashMap<String, ArrayList<Integer>> map = new HashMap<>();
 
@@ -18,21 +21,22 @@ public class GameStatsMain {
 
         addNumberSecondKey(map);
         addNumberThirdKey(map);
-        increaseTheNumberBy1(map);
+        incrementFirstCounter(map);
+        incrementSecondCounter(map);
 
-        ArrayList<Integer> firstKey = map.get("PVP");
-        for (Integer element : firstKey) {
+        ArrayList<Integer> firstValue = map.get("PVP");
+        for (Integer element : firstValue) {
             System.out.println(element);
         }
 
         ArrayList<Integer> secondKey = map.get("PVC1");
         for (Integer element : secondKey) {
-            System.out.println(element);
+        //    System.out.println(element);
         }
 
         ArrayList<Integer> thirdKey = map.get("PVC2");
         for (Integer element : thirdKey) {
-            System.out.println(element);
+        //    System.out.println(element);
         }
 
         Object objectMap = map;
@@ -54,11 +58,20 @@ public class GameStatsMain {
         list.add(5);
     }
 
-    public static void increaseTheNumberBy1(HashMap<String, ArrayList<Integer>> map) {
+    public static void incrementFirstCounter(HashMap<String, ArrayList<Integer>> map) {
 
         ArrayList<Integer> list = map.get("PVP");
         int number = list.get(0);
         list.set(0, number + 1);
+        map.put("PVP", list);
+
+    }
+
+    public static void incrementSecondCounter(HashMap<String, ArrayList<Integer>> map) {
+
+        ArrayList<Integer> list = map.get("PVP");
+        int number = list.get(1);
+        list.set(1, number + 1);
         map.put("PVP", list);
 
     }
